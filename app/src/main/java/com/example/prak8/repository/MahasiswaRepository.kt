60032394edee7652c9a7b2021a51acb8a6781827
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-30 04:40:34 +0700

    untuk melakukan operasi CRUD pada data mahasiswa melalui API

---
+++ app/src/main/java/com/example/prak8/repository/MahasiswaRepository.kt
@@ -0,0 +1,44 @@
+package com.example.prak8.repository
+
+import com.example.prak8.model.Mahasiswa
+import com.example.prak8.service_api.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository{
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaByNim(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim,mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP status code : ${response.code()} ")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaByNim(nim)
+    }
+}
